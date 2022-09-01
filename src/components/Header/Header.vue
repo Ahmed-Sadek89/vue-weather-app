7143200b1fda69e1de25e528d72e2dbaa9c662c0
Ahmed-Sadek89 <ahmeamohamed161@gmail.com>
2022-09-01 22:52:41 +0200

    vue vue-router ts tailwindCSS vue-animation & transition

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <div class="container header-content">
+      <div class="left" @click="redirectHome">
+        <i class="fa-solid fa-gear"></i>
+        <p>the local weather</p>
+      </div>
+      <div class="right">
+        <i class="fa-solid fa-circle-info"></i>
+        <i class="fa-solid fa-plus"></i>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+import './Styles.css';
+
+export default defineComponent({
+  name: 'Header',
+  setup() {
+    const router = useRouter();
+    const redirectHome = () => router.push({name: 'home'});
+
+    return {
+      redirectHome
+    }
+  }
+});
+</script>
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>home</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import './Styles.css';
+
+export default defineComponent({
+  name: 'Header',
+  
+});
+</script>
